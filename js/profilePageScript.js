8ea58c5597c4707fde266cce85502e69d0268bdd
Omar <om744981@gmail.com>
2026-08-07 22:09:11 +0300

    Get user's info dynamically

--- js/profilePageScript.js
+++ js/profilePageScript.js
@@ -1,1 +1,13 @@
 setupUI();
+
+async function getUser(){
+  const response = await axios.get(`${baseUrl}/users/2020`);
+  console.log(response.data.data);
+  const user = response.data.data;
+  document.getElementById("main-info-email").innerHTML = user.email;
+  document.getElementById("main-info-name").innerHTML = user.name;
+  document.getElementById("main-info-username").innerHTML = user.username;
+  document.getElementById("posts-count").innerHTML = user.posts_count;
+  document.getElementById("comments-count").innerHTML = user.comments_count;
+}
+getUser();
